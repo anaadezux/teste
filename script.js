03f836e71472796cc967c15d587233ec1b7e3a34
anaadezux <agacha093@gmail.com>
2025-01-25 21:41:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert(' Ownnnn, eu sabia que vc n ia recusar!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Ah, que pena... Mas tudo bem, a gente marca seu velorio ;)');
+});
